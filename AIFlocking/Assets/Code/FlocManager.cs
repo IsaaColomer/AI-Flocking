225542121ebec9c074eb721e948d799d37632bc1
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2021-10-09 18:21:50 +0200

    Not our code, must delete

--- AIFlocking/Assets/Code/FlocManager.cs
+++ AIFlocking/Assets/Code/FlocManager.cs
@@ -4,71 +4,94 @@ using UnityEngine;
 
 public class FlocManager : MonoBehaviour
 {
-    [Header("Flock Manager")]
-    [SerializeField] private GameObject fishPrefab;
-    public int numFish;
+    //[Header("Flock Manager")]
+    //[SerializeField] private GameObject fishPrefab;
+    //public int numFish;
 
-    [Header("Flock")]
+    //[Header("Flock")]
+    //public GameObject[] allFish;
+    //public float neighbourDistance;
+    //public float speed;
+    //public float rotationSpeed;
+
+    //[Header("Timer")]
+    //public float timeCount;
+    //public float timeMax;
+
+    //public Vector3 swimLimits;
+    //bool bounded;
+    //bool randomize;
+    //bool followLider;
+    //[SerializeField] GameObject lider;
+
+    //[Range(0f, 5f)]
+    //public float minSpeed;
+    //[Range(0f, 5f)]
+    //public float maxSpeed;
+    //[Range(0f, 5f)]
+    //public float NeighbourDistance;
+    //[Range(0f, 5f)]
+    //public float RotationSpeed;
+
+    //public GameObject acumulator;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    allFish = new GameObject[numFish];
+    //    for (int i = 0; i < numFish; ++i) {
+    //        Vector3 pos = this.transform.position + new Vector3(Random.Range(0, swimLimits.x)- swimLimits.x/2, Random.Range(0, swimLimits.y)- swimLimits.y/2, Random.Range(0, swimLimits.z)- swimLimits.z/2); // random position
+    //        Vector3 randomize = new Vector3(Random.Range(1,2), Random.Range(1,2), Random.Range(1,2)).normalized; // random vector direction
+    //        allFish[i] = (GameObject)Instantiate(fishPrefab, pos, 
+    //                                    Quaternion.LookRotation(randomize),acumulator.transform);
+    //        allFish[i].GetComponent<Flock>().myManager = this;
+    //        allFish[i].GetComponent<Flock>().speed = speed;
+    //    }
+
+    //    timeCount = timeMax;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //Recalculate();
+    //}
+
+    //void Recalculate()
+    //{
+
+    //}
+
+    public GameObject fishPrefab;
+    public int numFish = 20;
     public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
     public float neighbourDistance;
-    public float speed;
     public float rotationSpeed;
-
-    [Header("Timer")]
-    public float timeCount;
-    public float timeMax;
-
-    public Vector3 swimLimits;
-    bool bounded;
-    bool randomize;
-    bool followLider;
-    [SerializeField] GameObject lider;
-
-    [Range(0f, 5f)]
+    [Header("Fish Settings")]
+    [Range(0.0f, 5.0f)]
     public float minSpeed;
-    [Range(0f, 5f)]
+    [Range(0.0f, 5.0f)]
     public float maxSpeed;
-    [Range(0f, 5f)]
-    public float NeighbourDistance;
-    [Range(0f, 5f)]
-    public float RotationSpeed;
 
-    public GameObject acumulator;
-
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
         allFish = new GameObject[numFish];
-        for (int i = 0; i < numFish; ++i) {
-            Vector3 pos = this.transform.position + new Vector3(Random.Range(0, swimLimits.x)- swimLimits.x/2, Random.Range(0, swimLimits.y)- swimLimits.y/2, Random.Range(0, swimLimits.z)- swimLimits.z/2); // random position
-            Vector3 randomize = new Vector3(Random.Range(1,2), Random.Range(1,2), Random.Range(1,2)).normalized; // random vector direction
-            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, 
-                                        Quaternion.LookRotation(randomize),acumulator.transform);
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                  Random.Range(-swimLimits.y, swimLimits.y),
+                                                                  Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             allFish[i].GetComponent<Flock>().myManager = this;
-            allFish[i].GetComponent<Flock>().speed = speed;
         }
 
-        timeCount = timeMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Recalculate();
-    }
-
-    void Recalculate()
-    {
-        //timeCount -= Time.deltaTime;
-        //if(timeCount <= 0)
-        //{
-        //    for (int i = 0; i < allFish.Length; i++)
-        //    {
-        //        allFish[i].GetComponent<Flock>().Hello();
-        //    }
-
-        //    timeCount = timeMax;
-        //}
 
     }
 }
--- AIFlocking/Assets/Code/Flock.cs
+++ AIFlocking/Assets/Code/Flock.cs
@@ -4,104 +4,168 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    //public FlocManager myManager;
+    //public Vector3 direction;
+    //public float speed;
 
+    //public float timeCount;
+    //public float timeMax;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    timeCount = timeMax;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(direction),myManager.rotationSpeed * Time.deltaTime);
+    //    transform.Translate(-(Time.deltaTime * speed),0.0f, 0.0f);
+    //    //direction = (Cohesion() + Align() + Separation()).normalized * speed;
+    //    //Hello();
+    //}
+
+    //public Vector3 Cohesion()
+    //{
+    //    Vector3 cohesion = Vector3.zero;
+    //    int num = 0;
+    //    foreach (GameObject go in myManager.allFish)
+    //    {
+    //        if (go != this.gameObject)
+    //        {
+    //            float distance = Vector3.Distance(go.transform.position,
+    //                                              transform.position);
+    //            if (distance <= myManager.neighbourDistance)
+    //            {
+    //                cohesion += go.transform.position;
+    //                num++;
+    //            }
+    //        }
+    //    }
+    //    if (num > 0)
+    //        cohesion = (cohesion / num - transform.position).normalized * speed;
+
+    //    return cohesion;
+    //}
+
+    //public Vector3 Align()
+    //{
+    //    Vector3 align = Vector3.zero;
+    //    int num = 0;
+    //    foreach (GameObject go in myManager.allFish)
+    //    {
+    //        if (go != this.gameObject)
+    //        {
+    //            float distance = Vector3.Distance(go.transform.position,
+    //                                              transform.position);
+    //            if (distance <= myManager.neighbourDistance)
+    //            {
+    //                align += go.GetComponent<Flock>().direction;
+    //                num++;
+    //            }
+    //        }
+    //    }
+    //    if (num > 0)
+    //    {
+    //        align /= num;
+    //        speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
+    //    }
+
+    //    return align;
+    //}
+
+    //public Vector3 Separation()
+    //{
+    //    Vector3 separation = Vector3.zero;
+    //    foreach (GameObject go in myManager.allFish)
+    //    {
+    //        if (go != this.gameObject)
+    //        {
+    //            float distance = Vector3.Distance(go.transform.position,
+    //                                              transform.position);
+    //            if (distance <= myManager.neighbourDistance)
+    //                separation -= (transform.position - go.transform.position) /
+    //                              (distance * distance);
+    //        }
+    //    }
+
+    //    return separation;
+    //}
+
+    //public void Hello()
+    //{
+    //    timeCount -= Time.deltaTime;
+    //    if (timeCount < 0)
+    //    {
+    //        foreach (GameObject go in myManager.allFish)
+    //        {
+    //            direction = (Cohesion() + Align() + Separation()).normalized * speed;
+    //        }
+    //        timeCount = timeMax;
+    //    }
+    //}
     public FlocManager myManager;
-    public Vector3 direction;
-    public float speed;
+    float speed;
 
-    public float timeCount;
-    public float timeMax;
-
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
+        speed = Random.Range(myManager.minSpeed,
+                                myManager.maxSpeed);
 
-        timeCount = timeMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(direction),myManager.rotationSpeed * Time.deltaTime);
-        transform.Translate(-(Time.deltaTime * speed),0.0f, 0.0f);
-        //direction = (Cohesion() + Align() + Separation()).normalized * speed;
-        Hello();
-    }
+        transform.Translate(-Time.deltaTime * speed, 0, 0);
+        ApplyRules();
 
-    public Vector3 Cohesion()
+    }
+    void ApplyRules()
     {
-        Vector3 cohesion = Vector3.zero;
-        int num = 0;
-        foreach (GameObject go in myManager.allFish)
+        GameObject[] gos;
+        gos = myManager.allFish;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach (GameObject go in gos)
         {
             if (go != this.gameObject)
             {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
-                if (distance <= myManager.neighbourDistance)
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (nDistance <= myManager.neighbourDistance)
                 {
-                    cohesion += go.transform.position;
-                    num++;
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if (nDistance < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
                 }
             }
         }
-        if (num > 0)
-            cohesion = (cohesion / num - transform.position).normalized * speed;
 
-        return cohesion;
-    }
-
-    public Vector3 Align()
-    {
-        Vector3 align = Vector3.zero;
-        int num = 0;
-        foreach (GameObject go in myManager.allFish)
+        if (groupSize > 0)
         {
-            if (go != this.gameObject)
-            {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
-                if (distance <= myManager.neighbourDistance)
-                {
-                    align += go.GetComponent<Flock>().direction;
-                    num++;
-                }
-            }
-        }
-        if (num > 0)
-        {
-            align /= num;
-            speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
-        }
+            vcentre = vcentre / groupSize;
+            speed = gSpeed / groupSize;
 
-        return align;
-    }
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                      Quaternion.LookRotation(direction),
+                                                      myManager.rotationSpeed * Time.deltaTime);
 
-    public Vector3 Separation()
-    {
-        Vector3 separation = Vector3.zero;
-        foreach (GameObject go in myManager.allFish)
-        {
-            if (go != this.gameObject)
-            {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
-                if (distance <= myManager.neighbourDistance)
-                    separation -= (transform.position - go.transform.position) /
-                                  (distance * distance);
-            }
-        }
-
-        return separation;
-    }
-
-    public void Hello()
-    {
-        timeCount -= Time.deltaTime;
-        if (timeCount < 0)
-        {
-            direction = (Cohesion() + Align() + Separation()).normalized * speed*Time.deltaTime;
-            timeCount = timeMax;
         }
     }
 }
