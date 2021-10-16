1a87586ab167654a6ab748afc1e2aa25445747a3
MHF13 <41444696+MHF13@users.noreply.github.com>
2021-10-16 17:32:23 +0200

    Update Flock.cs

---
+++ AIFlocking/Assets/Code/Flock.cs
@@ -0,0 +1,194 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlocManager myManager;
+    public ROtateARound sphere;
+    public Vector3 direction;
+    public float yPos;
+    public float speed;
+    public bool can;
+    public float timeCount;
+    public float timeMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCount = timeMax;
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        if ((myManager.transform.position - transform.position).magnitude >= myManager.limit)
+        {
+            can = true;
+        }
+        else
+        {
+            can = false;
+        }
+        if(can)
+        { 
+            direction = (-(transform.right - (myManager.transform.position - transform.position)));
+            if(transform.position.y > 0)
+            {
+                float lols = 0;
+                lols += 0.01f;
+                direction = direction + new Vector3(0, transform.position.y + lols, 0);
+            }
+            else
+            {
+                float lols = 0;
+                lols += 0.01f;
+                direction = direction + new Vector3(0, transform.position.y - lols, 0);
+            }
+
+        }
+        else
+        {
+            direction = ((Cohesion() + Align() + Separation()).normalized * speed);
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        transform.Translate((Time.deltaTime * speed), 0.0f, 0.0f);
+
+        Debug.Log((transform.position - myManager.transform.position).magnitude);
+    }
+    public Vector3 Cohesion()
+    {
+        Vector3 cohesion = Vector3.zero;
+        int num = 0;
+        foreach (GameObject go in myManager.allFish)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position,
+                                                  transform.position);
+                if (distance <= myManager.neighbourDistance)
+                {
+                    cohesion += go.transform.position;
+                    num++;
+                }
+            }
+        }
+        if (num > 0)
+            cohesion = (cohesion / num - transform.position).normalized * speed;
+
+        return cohesion;
+    }
+    public Vector3 Align()
+    {
+        Vector3 align = Vector3.zero;
+        int num = 0;
+        foreach (GameObject go in myManager.allFish)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position,
+                                                  transform.position);
+                if (distance <= myManager.neighbourDistance)
+                {
+                    align += go.GetComponent<Flock>().direction;
+                    num++;
+                }
+            }
+        }
+        if (num > 0)
+        {
+            align /= num;
+            speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
+        }
+
+        return align;
+    }
+    public Vector3 Separation()
+    {
+        Vector3 separation = Vector3.zero;
+        foreach (GameObject go in myManager.allFish)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position,
+                                                  transform.position);
+                if (distance <= myManager.neighbourDistance)
+                    separation -= (transform.position - go.transform.position) /
+                                  (distance * distance);
+            }
+        }
+        return separation;
+    }
+
+    void Evade()
+    {
+        Vector3 targetDir = transform.position - (ROtateARound.instance.transform.position - transform.position);
+        //myManager.sphereLimit = targetDir.magnitude;
+        float lookAhead = targetDir.magnitude / speed;
+        transform.position = (-ROtateARound.instance.transform.position - ROtateARound.instance.transform.forward * lookAhead);
+    }
+
+
+    //public FlocManager myManager;
+    //float speed;
+
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    speed = Random.Range(myManager.minSpeed,
+    //                            myManager.maxSpeed);
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Translate(-Time.deltaTime * speed, 0, 0);
+    //    ApplyRules();
+
+    //}
+    //void ApplyRules()
+    //{
+    //    GameObject[] gos;
+    //    gos = myManager.allFish;
+
+    //    Vector3 vcentre = Vector3.zero;
+    //    Vector3 vavoid = Vector3.zero;
+    //    float gSpeed = 0.01f;
+    //    float nDistance;
+    //    int groupSize = 0;
+
+    //    foreach (GameObject go in gos)
+    //    {
+    //        if (go != this.gameObject)
+    //        {
+    //            nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+    //            if (nDistance <= myManager.neighbourDistance)
+    //            {
+    //                vcentre += go.transform.position;
+    //                groupSize++;
+
+    //                if (nDistance < 1.0f)
+    //                {
+    //                    vavoid = vavoid + (this.transform.position - go.transform.position);
+    //                }
+
+    //                Flock anotherFlock = go.GetComponent<Flock>();
+    //                gSpeed = gSpeed + anotherFlock.speed;
+    //            }
+    //        }
+    //    }
+
+    //    if (groupSize > 0)
+    //    {
+    //        vcentre = vcentre / groupSize;
+    //        speed = gSpeed / groupSize;
+
+    //        Vector3 direction = (vcentre + vavoid) - transform.position;
+    //        if (direction != Vector3.zero)
+    //            transform.rotation = Quaternion.Slerp(transform.rotation,
+    //                                                  Quaternion.LookRotation(direction),
+    //                                                  myManager.rotationSpeed * Time.deltaTime);
+
+    //    }
+    //}
+}
